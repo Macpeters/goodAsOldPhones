505560b3cabadf225f836568e4aa62ee28da896f
Macpeters <peters.maryanne@gmail.com>
2016-06-26 15:47:59 -0400

    all done with dynamic images and text (mvp)

---
+++ GoodAsOldPhones/Product.swift
@@ -0,0 +1,17 @@
+//
+//  Product.swift
+//  GoodAsOldPhones
+//
+//  Created by Maryanne Peters on 2016-06-26.
+//  Copyright © 2016 Maryanne Peters. All rights reserved.
+//
+
+import Foundation
+
+class Product {
+
+    var name: String?
+    var productImage: String?
+    var cellImage: String?
+    
+}
